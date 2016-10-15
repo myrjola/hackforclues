a770b014a790e1d3f6d3608bb4407cb83f0499cb
Martin Yrjölä <martin.yrjola@gmail.com>
2016-10-15 17:51:21 +0200

    Implement throttling

--- jsondumpster/settings.py
+++ jsondumpster/settings.py
@@ -127,9 +127,16 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '60/minute'
+    }
 }
