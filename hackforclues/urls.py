ee2a484933e76e5328dbe45fcea225088083fed1
Martin Yrjölä <martin.yrjola@gmail.com>
2017-03-11 23:07:15 +0100

    Get rid of rest framework

--- hackforclues/urls.py
+++ hackforclues/urls.py
@@ -2,14 +2,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from rest_framework import routers
-
-router = routers.DefaultRouter()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-
-    url(r'^api/v1/', include(router.urls)),
-    url(r'^api/v1/api-auth/', include('rest_framework.urls',
-                                      namespace='rest_framework')),
 ]
